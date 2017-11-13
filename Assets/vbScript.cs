433b65e337e82eb2b56ba9cb50829d6ec29ec6c1
Rohit Kumar <Rohit.Kumar@efi.com>
2017-11-13 18:44:01 +0530

    First commit

---
+++ Assets/vbScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class vbScript : MonoBehaviour, IVirtualButtonEventHandler {
+
+	private GameObject vButtonObject;
+	private GameObject zombie;
+
+	// Use this for initialization
+	void Start () {
+		vButtonObject = GameObject.Find ("actionButton");
+		zombie = GameObject.Find ("zombie");
+		zombie.SetActive(false);
+		vButtonObject.GetComponent<VirtualButtonBehaviour> ().RegisterEventHandler (this);
+
+	}
+
+	public void OnButtonPressed (VirtualButtonAbstractBehaviour vb){
+
+		Debug.Log ("button down!!!!");
+		zombie.SetActive(true);
+		zombie.GetComponent<Animation> ().Play ();
+	}
+
+	public void OnButtonReleased (VirtualButtonAbstractBehaviour vb){
+
+		zombie.GetComponent<Animation> ().Stop ();
+	}
+
+}
